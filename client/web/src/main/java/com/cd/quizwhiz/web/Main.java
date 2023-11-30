f5c1638d57389403df4172eb3402f003f626458f
ConorBISE <conorb+ise@firstpartners.net>
2023-11-30 08:29:28 +0000

    Remove unused opt-killer code from web main

--- client/web/src/main/java/com/cd/quizwhiz/web/Main.java
+++ client/web/src/main/java/com/cd/quizwhiz/web/Main.java
@@ -1,29 +1,14 @@
 package com.cd.quizwhiz.web;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import com.cd.quizwhiz.client.ui.AppState;
 import com.cd.quizwhiz.client.ui.HomePage;
-import com.cd.quizwhiz.client.ui.LoginPage;
-import com.cd.quizwhiz.client.ui.SignupPage;
 import com.cd.quizwhiz.web.ui.WebNetClient;
 import com.cd.quizwhiz.web.ui.WebResourceLoader;
 import com.cd.quizwhiz.web.ui.WebUI;
 
 public class Main {
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-        //optKeeper(HomePage.class, LoginPage.class, SignupPage.class);
-        
+    public static void main(String[] args) {
         new WebUI<>(new AppState(), new WebResourceLoader(), new WebNetClient())
-            .loadPage(new HomePage());
+                .loadPage(new HomePage());
     }
-
-    /*static void optKeeper(Class<?>... classes) {
-        for (Class<?> c : classes) {
-            for (Method m : c.getMethods()) {
-                System.out.println(m.getAnnotations());
-            }
-        }
-    }*/
 }
